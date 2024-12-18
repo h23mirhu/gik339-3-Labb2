9d71ce2f066d7e7bd3dd79d5dedffd853c562350
h23mirhu <h23mirhu@du.se>
2024-12-18 14:32:20 +0100

    Update server.js

---
+++ server/server.js
@@ -0,0 +1,27 @@
+// Variable express imports the npm-package express
+const express = require('express');
+// Variable that holds the server object
+const server = express();
+
+// With server.use() the servers overall settings are set
+server
+    .use(express.json())
+    .use(express.urlencoded({ extended: false }))
+    .use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', '*');
+res.header('Access-Control-Allow-Methods', '*');
+next();
+});
+
+// The server is started with the method .listen on port 3000
+// A callbackfunktion that writes to the console when the server starts
+server.listen(3000, () => {
+    console.log('The server is running on http://localhost:3000');
+});
+
+// A .get method with the endpoint /users
+// The .get method has a callbackfunction in the form of an arrowfunktion with the parameters req and res
+server.get('/users', (req, res) => {
+
+});
