8dc6f74fdc92940f48d7bfe71c770fb705bb5a5b
h23mirhu <h23mirhu@du.se>
2024-12-19 14:18:25 +0100

    Update script.js

--- client/script.js
+++ client/script.js
@@ -7,10 +7,8 @@ const newUl = document.createElement('ul');
 newUl.classList.add('new-ul');
 // Adds background color to element
 newUl.classList.add('background');
-
+// Adds the ul-element in the div-elemnt
 div.insertAdjacentElement('beforeend', newUl);
-
-
 
 // Fetches data from the database
 fetch('http://localhost:3000/users')
@@ -19,18 +17,25 @@ fetch('http://localhost:3000/users')
     .then((response) => response.json())
     // The array is translated for the user-object with its properties id, firstName, lastName, username and color
     // The array is written to the console
+    // Returns the data
     .then((jsonData) => {
         console.log(jsonData);
         return jsonData;
     })
+    // Loops through the users-objects
     .then((users) => {
         users.forEach((user) => {
-            const newLi = document.createElement('li');            
-            newLi.classList.add(user.color);
+            // Creates a li-element for every user
+            const newLi = document.createElement('li');
+            // A class name is added to the element    
             newLi.classList.add('li');
+            // A background color is added to the element based on the users color
+            newLi.classList.add(user.color);
+            // A template string that writes the user's info in the li-element
             newLi.innerHTML = `Frist Name: ${user.firstName}<br />
                                Last Name: ${user.lastName}<br />
                                Username: ${user.username}`;
+            // Adds the li-element in the ul-elemnt
             newUl.insertAdjacentElement('beforeend', newLi);
         });
     });
